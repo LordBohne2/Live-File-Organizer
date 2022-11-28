154f5686e1e9c1ead3039d222e3d5d683abb3171
LordBohne2 <71585629+LordBohne2@users.noreply.github.com>
2022-11-28 10:09:49 +0100

    Create Project

---
+++ Live-File-Organizer/Live-File-Organizer/Program.cs
@@ -0,0 +1,10 @@
+using Live_File_Organizer;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
